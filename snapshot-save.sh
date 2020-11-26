ce47a96e4156fb8443668c5324c4ddf84a61fb39
Sophia Vu <41712745+hsvu@users.noreply.github.com>
2020-11-26 22:31:35 +1100

    init commit

---
+++ snapshot-save.sh
@@ -0,0 +1,20 @@
+#!/bin/dash
+
+next_backup=0
+while true
+do
+    if [ ! -d ".snapshot.$next_backup" ]; then
+        break
+    fi
+    next_backup=$(( $next_backup+1 ))
+done
+
+mkdir .snapshot.$next_backup
+for files in `ls`
+do
+    if [ ! $files = "snapshot-save.sh" ] && [ ! $files = "snapshot-load.sh" ]; then
+        cp $files .snapshot.$next_backup
+    fi
+done
+
+echo "Creating snapshot $next_backup"
